0f7bb6f1706e0884bd718e823a27019ce7e76d73
John Guest <jcguest90@gmail.com>
2019-08-22 04:29:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,21 +2,21 @@
 # returs true if number is prime
 
 
-# def prime?(num)
-#   (2..num - 1).each do |n|
-#     return true if num % n != 0
-    
-#   end
-#   false 
-# end
-
-
 def prime?(num)
-  if num < 0 or num == 0 or num == 1
-    return false
-  else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
-    end
+  (2..(num - 1)).to_a.each do |n|
+    return true if num % n != 0
+    
   end
+  false 
 end
+
+
+# def prime?(num)
+#   if num < 0 or num == 0 or num == 1
+#     return false
+#   else
+#     (2..num-1).to_a.all? do |possible_factor|
+#       num % possible_factor != 0
+#     end
+#   end
+# end
