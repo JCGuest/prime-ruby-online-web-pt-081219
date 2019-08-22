0c97a11f5fc04172f302d4a2f0abd76c960fe474
John Guest <jcguest90@gmail.com>
2019-08-22 04:10:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 # def prime?(numbers)
 # returs true if number is prime
-
+require 'pry'
 
 def prime?(num)
   (2..num).each do |n|
     return false if num % n == 0
+    binding.pry
   end
   true
 end
