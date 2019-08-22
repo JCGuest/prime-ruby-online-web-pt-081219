6a2d49411d7a8265ae33f31a4c60954745e4113c
John Guest <jcguest90@gmail.com>
2019-08-22 04:19:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,20 @@
 
 
 def prime?(num)
-  (1..(num - 1)).each do |n|
+  (2..num - 1).each do |n|
     return false if num % n == 0
     
   end
   true
 end
+
+
+# def prime?(num)
+#   if num < 0 or num == 0 or num == 1
+#     return false
+#   else
+#     (2..num-1).each do |i|
+#       num % i != 0
+#     end
+#   end
+# end
