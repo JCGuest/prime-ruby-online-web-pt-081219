2b805f1c476effb46c97c3f270b17d4c70d50e56
John Guest <jcguest90@gmail.com>
2019-08-22 04:05:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
-def prime?(numbers)
-returs true if number is prime
+# def prime?(numbers)
+# returs true if number is prime
 
 
 def prime?(num)
-
-
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
 end
