b85652f1bbb5b7871b2446694b7f7db53eb62d51
John Guest <jcguest90@gmail.com>
2019-08-22 04:30:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,13 @@
 # returs true if number is prime
 
 
-def prime?(num)
-  (2..(num - 1)).to_a.each do |n|
-    return true if num % n != 0
+# def prime?(num)
+#   (2..(num - 1)).to_a.each do |n|
+#     return true if num % n != 0
     
-  end
-  false 
-end
+#   end
+#   false 
+# end
 
 
 # def prime?(num)
